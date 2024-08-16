976f0643e639c934b8e08c3eb6a572cf33740e57
Alexandre Carvalho <alex380bb@gmail.com>
2024-08-16 12:32:40 -0300

    req 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [imageUrl, setImageUrl] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchDogApi = (() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json())
+      .then((data) => setImageUrl(data.message))
+      .then(() => setIsLoading(false));
+  });
+
+  useEffect(() => {
+    fetchDogApi();
+  }, []);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
   return (
     <div>
       <h1>Doguinhos</h1>
+      <button
+        type="button"
+        onClick={ fetchDogApi }
+      >
+        Novo doguinho!
+      </button>
+      <div>
+        <img src={ imageUrl } alt="Doguinho aleatório" />
+      </div>
     </div>
   );
 }
